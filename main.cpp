8a0b2f92da2852aca7aac0a22b2a0e3693eadb1e
Robin Rolf <imer@imer.cc>
2016-08-24 07:55:56 +0200

    Move background to engine

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "LD35.hpp"
 #include "Player.hpp"
-#include "Background.hpp"
 #include "Rune.hpp"
 #include "Goal.hpp"
 #include "Tip.hpp"
@@ -11,7 +10,6 @@
 
 int main() {
 	engine::Factory::RegisterType("player", engine::Factory::CreateChildNode<Player>);
-	engine::Factory::RegisterType("bg", engine::Factory::CreateChildNode<Background>);
 	engine::Factory::RegisterType("rune", engine::Factory::CreateChildNode<Rune>);
 	engine::Factory::RegisterType("goal", engine::Factory::CreateChildNode<Goal>);
 	engine::Factory::RegisterType("tip", engine::Factory::CreateChildNode<Tip>);
